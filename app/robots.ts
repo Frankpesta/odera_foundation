b11f436cbaf7a5bedd7bb958725bc92327c009e1
Frankpesta <franklinolisaemeka3@gmail.com>
2025-07-20 10:43:03 +0100

    refactor: update sitemap URL in robots file and change event status in sitemap function

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
 			allow: "/",
 			disallow: ["/admin/", "/api/"],
 		},
-		sitemap: "https://oderahelpinghandsfoundation.org/sitemap.xml",
+		sitemap: "https://oderahelpinghandsfoundation.org/sitemap.ts",
 	};
 }
